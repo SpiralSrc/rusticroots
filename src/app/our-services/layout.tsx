b457feac7f502d99bd48a54f17cd90e88efc2016
Wyen GW <spiral@Spiral.local>
2024-03-19 14:40:58 -0700

    services page layout image bg

--- src/app/our-services/layout.tsx
+++ src/app/our-services/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import React from "react";
+import imgPng from "../../../public/assets/B99F15D5-3C00-4D1C-98A2-2275EF6CE171.png";
 
 export const metadata: Metadata = {
   title: "Our Services",
@@ -11,7 +13,11 @@ export default function layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-screen h-screen bg-gradient-theme2">
+    <div className="w-screen h-screen bg-gradient-theme2 relative">
+      <div className="fixed top-0 left-0 -z-10 w-[40%] h-full">
+        <Image src={imgPng} alt="image" fill className="opacity-40" />
+      </div>
+
       <section className="max-w-7xl m-auto">{children}</section>
     </div>
   );
